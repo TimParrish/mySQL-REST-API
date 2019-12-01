70f8727eff76fcdfd32bf07dfa404fb8126a85c9
Tim Parrish <timothyjparrish@gmail.com>
2019-12-01 13:59:09 -0700

    successful response from mySQL on server to web browser client

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 const expressInstance = require("express");
 const serverApplication = expressInstance();
+const mysql = require("mysql");
 
 serverApplication.get("/", (req, res) => {
   console.log("Hit the root route");
-  res.send("first working local");
+  res.send("Use a more specific route");
 });
 
-serverApplication.get("/states", (req, res) => {
-  console.log("sending the crime data for states!");
-  res.send("this will send states data");
+serverApplication.get("/test", (req, res) => {
+  console.log("get test from database");
+
+  const databaseConnection = mysql.createConnection({
+    host: "localhost",
+    user: "api",
+    password: "password",
+    database: "timstest",
+    insecureAuth: true
+  });
+
+  const testQuery = "select * from people";
+
+  databaseConnection.query(testQuery, (err, rows, fields) => {
+    console.log("test retrieved???");
+    res.json(rows);
+	if (err){
+		console.log("database error: " + err);
+		res.sendStatus(500);
+	};
+  });
 });
 
 serverApplication.get("/region", (req, res) => {
@@ -16,7 +35,7 @@ serverApplication.get("/region", (req, res) => {
   res.send("this will send region data");
 });
 
-//Server will listen on port 3027
-serverApplication.listen(3027, () => {
-  console.log("Server initialized!!!");
+//Server will listen on port 443
+serverApplication.listen(443, () => {
+  console.log("Server initialized and listening on port 443!!!");
 });
