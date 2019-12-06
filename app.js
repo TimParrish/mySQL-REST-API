c672bdbb265151054cc7df06ce3bb15f76e161fc
Tim Parrish <timothyjparrish@gmail.com>
2019-12-06 13:00:30 -0700

    expanded routes

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const expressInstance = require("express");
 const serverApplication = expressInstance();
 const mysql = require("mysql");
 const cors = require("cors");
-
 serverApplication.use(cors());
 
 //get VIOLENT crime data for a given state by year
@@ -34,7 +33,41 @@ serverApplication.get("/violent/:stateName/:crime/:dataYear", (req, res) => {
 
     return res.json(rows);
   });
-});
+}); //END: get VIOLENT crime data for a given state by year
+
+//get NON-VIOLENT crime data for a given state by year
+serverApplication.get(
+  "/non-violent/:stateName/:crime/:dataYear",
+  (req, res) => {
+    console.log("get state test from database");
+
+    const databaseConnection = mysql.createConnection({
+      host: "localhost",
+      user: "parrish",
+      password: "password121419",
+      database: "parrish",
+      insecureAuth: true
+    });
+    const stateName = req.params.stateName;
+    const dataYear = req.params.dataYear;
+    const crime = req.params.crime;
+
+    const selectStateQuery = `SELECT ${crime} FROM STATE INNER JOIN NON_VIOLENT_CRIME ON STATE.State_id = NON_VIOLENT_CRIME.State_id AND STATE.Data_for_year = NON_VIOLENT_CRIME.Data_for_year WHERE State_name = '${stateName}' and STATE.Data_for_year = ${dataYear}`;
+
+    console.log("\n" + selectStateQuery + "\n");
+
+    databaseConnection.query(selectStateQuery, (err, rows, fields) => {
+      console.log("State data was retrieved!!!");
+
+      if (err) {
+        console.log("database error: " + err);
+        res.sendStatus(500);
+      }
+
+      return res.json(rows);
+    });
+  }
+); //END: get NON-VIOLENT crime data for a given state by year
 
 // Get MAX data for a given crime in a given year
 serverApplication.get("/maxData/:crime/:year", (req, res) => {
@@ -63,9 +96,8 @@ serverApplication.get("/maxData/:crime/:year", (req, res) => {
     }
 
     return res.json(rows);
-
   });
-});
+}); // END: Get MAX data for a given crime in a given year
 
 // Get MIN data for a given crime in a given year
 serverApplication.get("/minData/:crime/:year", (req, res) => {
@@ -94,81 +126,74 @@ serverApplication.get("/minData/:crime/:year", (req, res) => {
     }
 
     return res.json(rows);
-
   });
-});
+}); // END: Get MIN data for a given crime in a given year
 
-// TODO
-// get crime data by region
-serverApplication.get("/region/:regionName/", (req, res) => {
-  console.log("sending the crime data for region!");
-  res.send("this will send region data");
+// See aggregate national totals by year
+serverApplication.get("/nationalAggregateTotal/:year", (req, res) => {
+  console.log("sending the aggregate national crime totals!");
 
-/*  const databaseConnection = mysql.createConnection({
-    host: "localhost",
-    user: "parrish",
-    password: "password121419",
-    database: "parrish",
-    insecureAuth: true
-  });
-  const stateName = req.params.stateName;
-  const dataYear = req.params.dataYear;
-  const selectStateQuery = "select * from STATE where State_name = ? and Data_for_year = ?";
+  //TODO: remove next line after query written
+  res.send("this will send aggregate national data");
 
-  databaseConnection.query(selectStateQuery, [stateName, dataYear], (err, rows, fields) => {
-    console.log("State data was retrieved!!!");
-    res.json(rows);
+  //const dataYear = req.params.year;
 
-    if (err) {
-      console.log("database error: " + err);
-      res.sendStatus(500);
-    }
-  });*/
-});
+  //TODO: MAKE A VALID QUERY TO SHOW ALL TOTALS NATIONALY AND RETURN DATA
+  // const selectStateQuery =
+  //   "select * from STATE where State_name = ? and Data_for_year = ?";
+
+  // databaseConnection.query(
+  //   selectStateQuery,
+  //   [stateName, dataYear],
+  //   (err, rows, fields) => {
+  //     console.log("State data was retrieved!!!");
+  //     res.json(rows);
+
+  //     if (err) {
+  //       console.log("database error: " + err);
+  //       res.sendStatus(500);
+  //     }
+  //   }
+  // );
+}); // END: See aggregate national totals by year
 
 serverApplication.get("/sub-region", (req, res) => {
   console.log("sending the crime data for sub-region!");
   res.send("this will send sub-region data");
 });
 
-serverApplication.get("/violent/:crime/:stateName/:dataYear", (req, res) => {
-  console.log("sending the violent crime data for state!");
+// serverApplication.get("/violent/:crime/:stateName/:dataYear", (req, res) => {
+//   console.log("sending the violent crime data for state!");
 
-  const databaseConnection = mysql.createConnection({
-    host: "localhost",
-    user: "parrish",
-    password: "password121419",
-    database: "parrish",
-    insecureAuth: true
-  });
+//   const databaseConnection = mysql.createConnection({
+//     host: "localhost",
+//     user: "parrish",
+//     password: "password121419",
+//     database: "parrish",
+//     insecureAuth: true
+//   });
 
-  const state = req.params.stateName;
-  const crime = req.params.crime;
-  const year = req.params.dataYear;
+//   const state = req.params.stateName;
+//   const crime = req.params.crime;
+//   const year = req.params.dataYear;
 
-  const violentCrimeQuery = `select ${crime} FROM VIOLENT_CRIME WHERE State_id = ( Select State_id from STATE WHERE State_name = '${state}' and Data_for_year = ${year}) and Data_for_year = ${year}`;
+//   const violentCrimeQuery = `select ${crime} FROM VIOLENT_CRIME WHERE State_id = ( Select State_id from STATE WHERE State_name = '${state}' and Data_for_year = ${year}) and Data_for_year = ${year}`;
 
-  console.log("\n\n" + violentCrimeQuery + "\n\n");
+//   console.log("\n\n" + violentCrimeQuery + "\n\n");
 
-  //const violentCrimeQuery = "select Murder_manslaughter FROM VIOLENT_CRIME WHERE State_id = ( Select State_id from STATE where State_name = 'Montana' and Data_for_year = 2016) and Data_for_year = 2016";
+//   //const violentCrimeQuery = "select Murder_manslaughter FROM VIOLENT_CRIME WHERE State_id = ( Select State_id from STATE where State_name = 'Montana' and Data_for_year = 2016) and Data_for_year = 2016";
 
-  //databaseConnection.query(violentCrimeQuery, [crime, state, year, year], (err, rows, fields) => {
-  databaseConnection.query(violentCrimeQuery, (err, rows, fields) => {
-    console.log("Violent crime data for a given state and year was retrieved!!!");
-    res.json(rows);
+//   //databaseConnection.query(violentCrimeQuery, [crime, state, year, year], (err, rows, fields) => {
+//   databaseConnection.query(violentCrimeQuery, (err, rows, fields) => {
+//     console.log("Violent crime data for a given state and year was retrieved!!!");
+//     res.json(rows);
 
-    if (err) {
-      console.log("database error: " + err);
-      res.sendStatus(500);
-    }
-  });
-});
-
-
-serverApplication.get("/state", (req, res) => {
-  console.log("sending the crime data for state!");
-  res.send("this will send state data");
-});
+//     if (err) {
+//       console.log("database error: " + err);
+//       res.sendStatus(500);
+//     }
+//   });
+// });
 
 //Server will listen on port 8080
 serverApplication.listen(8080, () => {
